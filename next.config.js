d44568eef3cff2e2c8d5cb70c2ca0abef9d2b8b6
Duncan Conduit <dev@duncanconduit.com>
2023-07-17 15:18:13 +0100

    🎉 First Commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental: {
+        headers() {
+          return [
+            {
+              source: "/.well-known/apple-app-site-association",
+              headers: [{ key: "content-type", value: "application/json" }]
+            }
+          ];
+        }
+      }
+
+}
 
 module.exports = nextConfig
